948dd1f66d2c2e8cd59d3766b10123fc14122297
Kality <anthony-bailly@live.fr>
2022-11-06 23:36:57 +0100

    Prototype : init, impl, diagram

---
+++ GOF.Prototype/Customer.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF.Prototype
+{
+    public class Address
+    {
+        public string Address1 { get; set; } = null!;
+        public string Address2 { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string City { get; set; } = null!;
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            result.AppendLine($"{nameof(Address1)} = {Address1};");
+            result.AppendLine($"{nameof(Address2)} = {Address2};");
+            result.AppendLine($"{nameof(Country)} = {Country};");
+            result.AppendLine($"{nameof(City)} = {City};");
+            return result.ToString();
+        }
+
+    }
+
+    public class Customer 
+    {
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Guid Id { get; set; }
+        public Address Address { get; set; } = null!;
+
+        public Customer ShallowCopy()
+        {
+            return (Customer) this.MemberwiseClone();
+        }
+
+        public Customer DeepCopy()
+        {
+            Customer clone = (Customer) this.MemberwiseClone();
+            clone.Address = new Address()
+            {
+                Address1 = Address.Address1,
+                Address2 = Address.Address2,
+                City = Address.City,
+                Country = Address.Country,
+            };
+            return clone;
+        }
+    }
+}
---
+++ GOF.Prototype/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.Metrics;
+using GOF.Prototype;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        Customer c1 = new()
+        {
+            Age = 42,
+            BirthDate = Convert.ToDateTime("1977-01-01"),
+            Name = "Jack Daniels",
+            Address = new Address()
+            {
+                Address1 = "republique",
+                Address2 = "157 bat",
+                City = "Nanterre",
+                Country = "France"
+            }
+        };
+
+        // Perform a shallow copy of c1 and assign it to c2.
+        Customer c2 = c1.ShallowCopy();
+        // Make a deep copy of c1 and assign it to c3.
+        Customer c3 = c1.DeepCopy();
+
+        // Display values of c1, c2 and c3.
+        Console.WriteLine("Original values of c1, c2, c3:");
+        Console.WriteLine("c1 instance values: \n");
+        DisplayValues(c1);
+        Console.WriteLine("c2 instance values: \n");
+        DisplayValues(c2);
+        Console.WriteLine("c3 instance values: \n");
+        DisplayValues(c3);
+
+        // Change the value of c1 croperties and display the values of c1,
+        // c2 and c3.
+        c1.Age = 32;
+        c1.BirthDate = Convert.ToDateTime("1900-01-01");
+        c1.Name = "Frank";
+        c1.Address.Address1 = "Paris";
+        c1.Address.Address2 = "avion 113";
+        c1.Address.City = "Versaille";
+        c1.Address.Country = "France";
+
+        Console.WriteLine("\n Values of c1, c2 and c3 after changes to c1:");
+        Console.WriteLine("c1 instance values: \n");
+        DisplayValues(c1);
+        Console.WriteLine("c2 instance values (reference values have changed): \n");
+        DisplayValues(c2);
+        Console.WriteLine("c3 instance values (everything was kept the same): \n");
+        DisplayValues(c3);
+    }
+    public static void DisplayValues(Customer c)
+    {
+        Console.WriteLine($"Name: {c.Name},\nAge: {c.Age},\nBirthDate: {c.BirthDate}");
+        Console.WriteLine($"ID#: {c.Address}");
+    }    
+}
